b3be6b3cd805abf3823da0cd09db2a1498abf041
jslsn <jordan.s.layne@gmail.com>
2022-08-14 18:17:02 +0100

    Updated Web Deployment providers.tf comment.

--- Web_Deployment/terraform/providers.tf
+++ Web_Deployment/terraform/providers.tf
@@ -1,4 +1,4 @@
-#The providers tf file, this file defines the provider(s) we're using.
+#The providers tf file, this file defines the providers we're using and makes them easy to find.
 provider "aws" {
   region = "eu-west-1"
 }
